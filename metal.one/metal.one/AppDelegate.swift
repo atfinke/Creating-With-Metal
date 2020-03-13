3b26e48df83784612e9a41ebe30e62e6a398971e
Andrew Finke <andrewfinke@me.com>
2020-03-12 22:37:55 -0500

    inital samples

---
+++ metal.one/metal.one/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  metal.one
+//
+//  Created by Andrew Finke on 3/12/20.
+//  Copyright © 2020 Andrew Finke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ metal.one/metal.one/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  metal.one
+//
+//  Created by Andrew Finke on 3/12/20.
+//  Copyright © 2020 Andrew Finke. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
